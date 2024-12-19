d098cd27087b1d511203adc0128c6361aabc4704
silversun31 <silversunr@gmail.com>
2024-12-19 14:45:01 -0500

    chore: Create base urls file

---
+++ urls.ts
@@ -0,0 +1,9 @@
+// import {projectUrls} from "@/src/app/project/urls";
+// import {settingsUrls} from "@/src/app/settings/urls";
+
+
+// export const urlpatterns = {
+//     'index': '/',
+//     ...projectUrls,
+//     ...settingsUrls
+// }
