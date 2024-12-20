bad54c36742d0d328a4b287e91ccf61a7cffe5dd
silversun31 <silversunr@gmail.com>
2024-12-20 08:28:33 -0500

    feat: Filter products by category. Refactor components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,32 @@
 import {ChangeEvent, useContext, useEffect, useState} from "react";
 import {ApiServicesContext} from "@/core/context/ApiService/ApiServicesContext";
 import useNotificationService from "@/services/Notification";
-import {AddToCartButton} from "@/core/components/Cart/AddToCartButton";
+import ProductCard from "@/core/components/ProductCard";
+import {SearchAndFilter} from "@/core/components/Home/SearchAndFilter";
 
 export default function Home() {
     const [products, setProducts] = useState<Product[]>([]);
     const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
+    const [categories, setCategories] = useState<string[]>([]);
+    const [selectedCategory, setSelectedCategory] = useState<string>('all');
     const [searchQuery, setSearchQuery] = useState("");
     const {ProductApiService} = useContext(ApiServicesContext)
     const NotificationService = useNotificationService()
 
-    // Fetch products from Fake Store API
+    // Fetch products and categories from Fake Store API
     useEffect(() => {
         ProductApiService.getAll()
             .then((res) => {
                 setProducts(res.data)
                 setFilteredProducts(res.data)
+            })
+            .catch((err) => {
+                NotificationService.notify(err.message, {type: 'error'})
+            });
+
+        ProductApiService.getCategories()
+            .then((res) => {
+                setCategories(['all', ...res.data])
             })
             .catch((err) => {
                 NotificationService.notify(err.message, {type: 'error'})
@@ -27,44 +38,36 @@ export default function Home() {
     const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
         const query = e.target.value.toLowerCase();
         setSearchQuery(query);
-        const filtered = products.filter((product) =>
-            product.title.toLowerCase().includes(query)
-        );
+        filterProducts(query, selectedCategory);
+    };
+
+    // Handle category change
+    const handleCategoryChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        const category = e.target.value;
+        setSelectedCategory(category);
+        filterProducts(searchQuery, category);
+    };
+
+    // Filter products based on search query and selected category
+    const filterProducts = (query: string, category: string) => {
+        let filtered = products;
+        if (category !== 'all') {
+            filtered = filtered.filter(product => product.category === category);
+        }
+        if (query) {
+            filtered = filtered.filter(product => product.title.toLowerCase().includes(query));
+        }
         setFilteredProducts(filtered);
     };
 
     return (
         <div className="container mx-auto p-4">
-            <input
-                type="text"
-                placeholder="Buscar productos..."
-                value={searchQuery}
-                onChange={handleSearch}
-                className="input input-bordered w-full mb-4"
-            />
+            <SearchAndFilter searchQuery={searchQuery} handleSearch={handleSearch}
+                             handleCategoryChange={handleCategoryChange} categories={categories}
+                             selectedCategory={selectedCategory}/>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                 {filteredProducts.map((product) => (
-                    <div
-                        key={product.id}
-                        className="card shadow-lg transition"
-                    >
-                        <figure>
-                            <img
-                                src={product.image}
-                                alt={product.title}
-                                className="h-40 object-contain"
-                            />
-                        </figure>
-                        <div className="card-body items-center text-center">
-                            <h2 className="card-title">
-                                {product.title}
-                            </h2>
-                            <p className="text-gray-600">${product.price}</p>
-                            <div className="card-actions">
-                                <AddToCartButton product={product}/>
-                            </div>
-                        </div>
-                    </div>
+                    <ProductCard product={product} key={product.id}/>
                 ))}
             </div>
         </div>
