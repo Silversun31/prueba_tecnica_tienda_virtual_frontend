3218d4b51c2d6c51a651a84fe256252e0fc8db7a
silversun31 <silversunr@gmail.com>
2024-12-20 06:55:28 -0500

    chore: Install react icons dependency

--- src/core/components/NavBar.tsx
+++ src/core/components/NavBar.tsx
@@ -1,15 +1,16 @@
 import {APP_NAME} from "../../../app.config";
 import Link from "next/link";
 import {urlpatterns} from "../../../urls";
+import {BsCart3} from "react-icons/bs";
 
 export const NavBar = () => {
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-300">
             <div className="navbar-start">
                 <Link href={urlpatterns['index']} className="btn btn-ghost text-xl">{APP_NAME}</Link>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+                <a className="btn btn-ghost rounded-full"><BsCart3 size={18}/></a>
             </div>
         </div>
     );
