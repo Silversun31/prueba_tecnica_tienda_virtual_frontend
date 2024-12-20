88e8e7dcaa2b7c32ae56f56eec13547d17a10365
silversun31 <silversunr@gmail.com>
2024-12-19 20:06:57 -0500

    feat: Create "get categories"
    Create "getCategories" method in product api service

--- src/services/api/ProductApiService.ts
+++ src/services/api/ProductApiService.ts
@@ -1,11 +1,17 @@
 import BaseApiService from "@/services/api/BaseApiService";
-import {ApiProductGetAllResponse} from "@/models/api/ProductApiService";
+import {ApiProductGetAllResponse, ApiProductGetCategoriesResponse} from "@/models/api/ProductApiService";
 
 export class ProductApiService extends BaseApiService {
+    protected readonly SERVICE_URL: string = '/products';
 
     async getAll() {
-        const resource = '/products'
+        const resource = this.SERVICE_URL
         return await this.connect<ApiProductGetAllResponse>(resource, 'get')
+    }
+
+    async getCategories() {
+        const resource = `${this.SERVICE_URL}/categories`
+        return await this.connect<ApiProductGetCategoriesResponse>(resource, 'get')
     }
 }
 
